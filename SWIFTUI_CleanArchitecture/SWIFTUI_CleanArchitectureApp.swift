eb78c5e5d59ac9ca49f7f80cb661eb6c0836bf79
YSLee-Dev <ysleedev01@gmail.com>
2023-05-14 17:20:55 +0900

    Initial Commit

---
+++ SWIFTUI_CleanArchitecture/SWIFTUI_CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_CleanArchitectureApp.swift
+//  SWIFTUI_CleanArchitecture
+//
+//  Created by 이윤수 on 2023/05/14.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
